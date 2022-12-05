c0cc2432672f93be72d60d3467160cf3ad8033a2
Kim Robert <kimrobert.nb@gmail.com>
2022-12-05 17:34:44 +0100

    commit - feature databinding and ngif

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
 
+export class ServersComponent {
+  serverName = "Toto";
+  allowNewServer = false;
+  serverCreationStatus = "No server was created.";
+  serverCreated = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = "Server created with name --- " + this.serverName + " !";
+  }
 }
